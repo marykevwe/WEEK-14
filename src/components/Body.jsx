5bf617d7b26970986ee2d757fb8c31fd3f45e9df
Marian Aphiare <140634802+marykevwe@users.noreply.github.com>
2023-11-08 04:17:36 -0500

    create components folder

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+const Body = () => {
+  return (
+    <main>
+      <Card title="Card 1" content="Content for Card 1" />
+      <Card title="Card 2" content="Content for Card 2" />
+      <Card title="Card 3" content="Content for Card 3" />
+      <Card title="Card 4" content="Content for Card 4" />
+    </main>
+  );
+};
+
+export default Body;
---
+++ src/components/Xidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <ul>
+        <li>Dashboard</li>
+        <li>Profile</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
